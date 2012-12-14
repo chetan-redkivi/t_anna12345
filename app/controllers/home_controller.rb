04b80dce7ec661cc9b6614ca6642c12d92e3af15
Chetan <chetan@chetan.(none)>
2012-12-14 12:45:53 +0530

    http://t.co has been     end

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,29 +5,22 @@ class HomeController < ApplicationController
       twitter_authentication = current_user.twitter_authentication
       @new_client = Twitter::Client.new(:oauth_token => twitter_authentication.token,:oauth_token_secret => twitter_authentication.secret)
       @tweets  = @new_client.user_timeline(twitter_authentication.uid.to_i,:count => 100)
-      @filtered_tweets = []
+      @filtered_tweet_url = []
       @tweets.each do |tweet|
         tweet_text = {}
         new_arr_urls = []
-        arr_urls = URI.extract("#{tweet.text}")
-        if arr_urls.size > 0
-          arr_urls.each do |url|
-            if url[0..10] == "http://t.co"
-              if url[-1] == "."
-                url = url.chomp('.')
-              end
-              new_arr_urls << url
-            end
+        if tweet.urls.size > 0
+          tweet.urls.each do |url|
+            new_arr_urls << url.expanded_url
           end
         end
         unless new_arr_urls.blank?
-          tweet_text["text"] = new_arr_urls
+          tweet_text["url"] = new_arr_urls
         end
         unless tweet_text.blank?
-          @filtered_tweets << tweet_text
+          @filtered_tweet_url << tweet_text
         end
       end
-
     end
   end
 end
